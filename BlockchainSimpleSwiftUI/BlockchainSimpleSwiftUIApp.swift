e38a4cff72af3f5671019d4f24d6734055d46b59
Giorgio Giannotta <jorge_upa@msn.com>
2021-06-12 10:33:33 +0200

    Initial Commit

---
+++ BlockchainSimpleSwiftUI/BlockchainSimpleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockchainSimpleSwiftUIApp.swift
+//  BlockchainSimpleSwiftUI
+//
+//  Created by Jorge Giannotta on 12/06/21.
+//
+
+import SwiftUI
+
+@main
+struct BlockchainSimpleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
